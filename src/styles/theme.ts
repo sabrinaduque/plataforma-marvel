50efdc3529af628c241b31e294c6644dbc7994b1
Sabrina Duque <sabrinanduque@hotmail.com>
2024-05-15 01:56:10 -0300

    Atualizando theme colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,7 @@
 export default {
   COLORS: {
-    BACKGROUND_900: "#000", /*fundo da página*/
+    BACKGROUND_900: "#000000", /*fundo da página*/
+    WHITE: "#FFFFFF",
     RED: "#FF0000",
   }
 }
