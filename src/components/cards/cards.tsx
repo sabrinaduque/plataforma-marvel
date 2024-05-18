18cfdee879559113b74e2dc0e6c3cc4cdfa7d974
Sabrina Duque <sabrinanduque@hotmail.com>
2024-05-18 18:48:35 -0300

    Implementando versão inicial dos Cards

--- src/components/cards/cards.tsx
+++ src/components/cards/cards.tsx
@@ -1,8 +1,39 @@
-import { Container } from "./styles"
+import React, { useState } from "react";
+import { Container } from "./styles";
+import homemAranha from "../../statics/characters/Homem-Aranha.png";
 
 const Cards: React.FC = () => {
+  const [expanded, setExpanded] = useState<boolean>(false);
+
+  const handleVerDetalhesClick = () => {
+    setExpanded(!expanded)
+  };
+
   return (
-    <Container>
+    <Container className={expanded ? "expanded" : ""}>
+      <div className="content">
+        <img src={homemAranha} alt="Imagem do heroi" />
+
+        <div className="infos">
+          <p>Homem-Aranha</p>
+          <span className="description">
+          Após ser mordido por uma aranha radioativa, Peter Parker se torna o amigo da vizinhança, o Homem-Aranha.
+          </span>
+
+          {expanded && (
+          <div className="appears">
+            <span>
+              Aparições: Vingadores - Era de Ultron Capitão América - Guerra Civil Vingadores - Guerra Infinita Vingadores - Ultimato WandaVision
+            </span>
+          </div>
+          )}
+    
+          <button onClick={handleVerDetalhesClick}>
+            {expanded ? "Fechar Detalhes" : "Ver Detalhes"}
+          </button>
+       
+        </div>
+      </div>
     </Container>
   )
 }
--- src/components/cards/styles.ts
+++ src/components/cards/styles.ts
@@ -2,4 +2,63 @@ import styled from "styled-components"
 // import { DEVICE_BREAKPOINTS } from "../../styles"
 
 export const Container = styled.div`
+	display: inline-block;
+	position: relative;
+
+	.infos {
+		p {
+			padding-bottom: 20px;
+		}
+		
+		display: flex;
+		position: absolute;
+		top: 50%;
+		flex-direction: column;
+		align-items: center;
+		text-align: center;
+		padding: 20px;
+		height: 217px;
+		border-radius: 30px;
+		background: linear-gradient(180deg, 
+		#ff0000 0.00%, 
+		rgba(128,0,0,0.3 ) 100.00%);
+  }
+
+	&.expanded {
+		img {
+			position: absolute;
+			z-index: 1;
+		}
+
+		.infos {
+		.description {
+			display: none;
+		}
+
+		.appears {
+			span {
+				display: flex;
+				height: 274px;
+				width: 171px;
+			}
+		}
+		
+		display: flex;
+		position: relative;
+		top: 50%;
+		width: 607px;
+    padding-left: 304px;
+		height: 438px;
+		background: linear-gradient(90deg, 
+		#ff0000 0.00%, 
+		#400e0e 100.00%);
+		font-family: "Axiforma";
+		font-size: 18px;
+		font-weight: bold;
+		line-height: 22px;
+		font-style: normal;
+		text-align: left;
+		align-items: flex-start;
+  }
+	}
 `
