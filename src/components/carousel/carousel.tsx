b99e5510b88d579582fde038ac6cd6e30251811b
Sabrina Duque <sabrinanduque@hotmail.com>
2024-05-20 03:14:01 -0300

    Implementando Carousel

--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -1,9 +1,49 @@
-import { Container } from "./styles"
+import { Container, CarouselContainer } from "./styles"
+import React, { useRef } from "react";
+import arrowLeft from "../../statics/svg/arrow-left.svg";
+import arrowRight from "../../statics/svg/arrow-right.svg";
+import Cards from "../cards"
+import { isBrowser } from "react-device-detect";
+import { IDataCharacter} from "../../data/data-character"
+import { IDataComics } from "../../data/data-comics"
+import { IDataMovie } from "../../data/data-movies"
 
-const Carousel: React.FC = () => {
+interface CarouselProps {
+  data: IDataCharacter | IDataMovie | IDataComics
+}
+
+const Carousel: React.FC<CarouselProps> = ({data}) => {
+  const carouselContainer = useRef<HTMLDivElement>(null);
+  const dataCard = data
+
+  const handleLeftClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    e.preventDefault();
+    if (carouselContainer.current) {
+      carouselContainer.current.scrollLeft -= carouselContainer.current.offsetWidth;
+    }
+  };
+
+  const handleRightClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    e.preventDefault();
+    if (carouselContainer.current) {
+      carouselContainer.current.scrollLeft += carouselContainer.current.offsetWidth;
+    }
+  };
   return (
     <Container>
-      Carousel
+      <a href="#" onClick={handleLeftClick} className="reactIconLeft">
+        <img src={arrowLeft} alt="Previous" />
+      </a>
+      <CarouselContainer ref={carouselContainer}>
+        {isBrowser && (
+          <>
+            <Cards data={dataCard}></Cards>
+          </>
+        )}
+      </CarouselContainer>
+      <a href="#" onClick={handleRightClick} className="reactIconRight">
+        <img src={arrowRight} alt="Next" />
+      </a>
     </Container>
   )
 }
--- src/components/carousel/styles.ts
+++ src/components/carousel/styles.ts
@@ -1,5 +1,60 @@
-import styled from "styled-components"
-// import { DEVICE_BREAKPOINTS } from "../../styles"
+import styled from "styled-components";
+import { DEVICE_BREAKPOINTS } from "../../styles";
 
 export const Container = styled.div`
-`
+  max-width: 93vw;
+  flex: none;
+  position: relative;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media (max-width: ${DEVICE_BREAKPOINTS.MD}) {
+    max-width: 100vw;
+  }
+
+  .reactIconLeft {
+    padding-right: 84px;
+
+    @media (max-width: ${DEVICE_BREAKPOINTS.MD}) {
+      padding-right: 35px;
+    }
+    @media (max-width: ${DEVICE_BREAKPOINTS.SM}) {
+      display: none;
+    }
+  }
+
+  .reactIconRight {
+    padding-left: 84px;
+
+    @media (max-width: ${DEVICE_BREAKPOINTS.MD}) {
+      padding: 20px;
+    }
+
+    @media (max-width: ${DEVICE_BREAKPOINTS.SM}) {
+      display: none;
+    }
+  }
+`;
+
+export const CarouselContainer = styled.div`
+  display: flex;
+  overflow-x: auto;
+  scroll-behavior: smooth;
+  gap: 89px;
+
+  .buttons {
+    button {
+      background: none;
+      border: none;
+      cursor: pointer;
+      color: white;
+      width: 2rem;
+      height: 29.5rem;
+      z-index: 2;
+    }
+  }
+`;
