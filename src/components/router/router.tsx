48fa14c01aaef7452f12b05054d4498ebf5caa7c
Sabrina Duque <sabrinanduque@hotmail.com>
2024-05-20 03:29:20 -0300

    Implementando rota signOut

--- src/components/router/router.tsx
+++ src/components/router/router.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { SignIn, Characters, Movies, Comics } from '../../pages'
+import { SignIn, SignOut, Characters, Movies, Comics } from '../../pages'
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SignIn />} />
+        <Route path="/signOut" element={<SignOut />} />
         <Route path="/characters" element={<Characters />} />
         <Route path="/movies" element={<Movies />} />
         <Route path="/comics" element={<Comics />} />
