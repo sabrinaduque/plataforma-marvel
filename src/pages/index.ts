2c47559b7bd760e17bb3c89003cb85a66d596a21
Sabrina Duque <sabrinanduque@hotmail.com>
2024-05-20 03:19:04 -0300

    Implementação rota signOut

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,5 @@
 export { default as SignIn } from './sign-in'
+export { default as SignOut } from './sign-out'
 export { default as Characters } from './characters'
 export { default as Movies } from './movies'
 export { default as Comics } from './comics'
