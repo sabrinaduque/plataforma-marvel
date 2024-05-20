0e444d5aef566d5b26bd92e900d025e8ebf37abc
Sabrina Duque <sabrinanduque@hotmail.com>
2024-05-20 03:11:43 -0300

    Implementando card

--- src/components/cards/cards.tsx
+++ src/components/cards/cards.tsx
@@ -1,41 +1,126 @@
-import React, { useState } from "react";
-import { Container } from "./styles";
-import homemAranha from "../../statics/characters/Homem-Aranha.png";
+import React, { useState } from "react"
+import { Container, CardsWrapper, Overlay, FilterContainer, SortSelect } from "./styles"
+import closeButton from "../../statics/svg/close-button.svg"
+import emptyStar from "../../statics/empty-star.png"
+import fullStar from "../../statics/full-star.png"
+import { IDataCharacter} from "../../data/data-character"
+import { IDataComics } from "../../data/data-comics"
+import { IDataMovie } from "../../data/data-movies"
 
-const Cards: React.FC = () => {
-  const [expanded, setExpanded] = useState<boolean>(false);
-
-  const handleVerDetalhesClick = () => {
-    setExpanded(!expanded)
-  };
-
-  return (
-    <Container className={expanded ? "expanded" : ""}>
-      <div className="content">
-        <img src={homemAranha} alt="Imagem do heroi" />
-
-        <div className="infos">
-          <p>Homem-Aranha</p>
-          <span className="description">
-          Após ser mordido por uma aranha radioativa, Peter Parker se torna o amigo da vizinhança, o Homem-Aranha.
-          </span>
-
-          {expanded && (
-          <div className="appears">
-            <span>
-              Aparições: Vingadores - Era de Ultron Capitão América - Guerra Civil Vingadores - Guerra Infinita Vingadores - Ultimato WandaVision
-            </span>
-          </div>
-          )}
-    
-          <button onClick={handleVerDetalhesClick}>
-            {expanded ? "Fechar Detalhes" : "Ver Detalhes"}
-          </button>
-       
-        </div>
-      </div>
-    </Container>
-  )
+interface CardsProps {
+  data: IDataCharacter | IDataMovie | IDataComics
 }
 
-export default Cards
+const Cards: React.FC<CardsProps> = ({ data }) => {
+  const [expandedId, setExpandedId] = useState<number | null>(null);
+  const [sortOrder, setSortOrder] = useState<"release" | "chronology">("release");
+
+  const handleVerDetalhesClick = (id: number) => {
+    setExpandedId(prevId => (prevId === id ? null : id));
+  };
+
+  const renderStarRating = (rating: number) => {
+    const stars = [];
+    for (let i = 1; i <= 5; i++) {
+      if (i <= rating) {
+        stars.push(<img key={i} src={fullStar} alt="Full Star" className="star-image" />)
+      } else {
+        stars.push(<img key={i} src={emptyStar} alt="Empty Star" className="star-image" />)
+      }
+    }
+    return stars;
+  }
+
+  const sortedCardsData = [...data.data].sort((a, b) => {
+    if (sortOrder === "release") {
+      return a.release - b.release;
+    } else {
+      return a.chronology - b.chronology;
+    }
+  });
+
+  return (
+    <div>
+      {data.type === "movies" && (
+        <FilterContainer>
+          <SortSelect id="sortOrder" onChange={(e) => setSortOrder(e.target.value as "release" | "chronology")}>
+            <option>Filtrar por</option>
+            <option value="release">Lançamento</option>
+            <option value="chronology">Cronologia</option>
+          </SortSelect>
+        </FilterContainer>
+      )}
+
+      <CardsWrapper>
+        {expandedId !== null && <Overlay />}
+        {sortedCardsData.map((card) => (
+          <Container
+            key={card.id}
+            className={`${expandedId === card.id ? "expanded" : ""}`}
+          >
+            <div className="content">
+              <img src={card.image} alt={`Imagem do ${card.title}`} className="heroImage" />
+
+              <div className="infos">
+                <div className="title">
+                  <p>{card.title}</p>
+                </div>
+
+                <div className="description">
+                  <span>{card.description}</span>
+                </div>
+
+                {data.type === "characters" && expandedId === card.id && (
+                  <>
+                    <div className="appears">
+                      <span className="label-appearances"> Aparições: </span>
+                      <div className="appearances">
+                        {card.appearances.map((appearance, index) =>
+                          <div key={`${index}-appearance`}>{appearance}</div>
+                        )}
+                      </div>
+                    </div>
+                    <div className="classification">
+                      <span> Avaliações dos Fãs</span>
+                    </div>
+                    <div className="rating">
+                      {renderStarRating(card.rating)}
+                    </div>
+                  </>
+                )}
+
+                {(data.type === "movies" || data.type === "comics") && expandedId === card.id && (
+                  <>
+                    <div className="more">
+                      <div className="description-details">
+                        {card.description}
+                      </div>
+                    </div>
+                    <div className="streaming">
+                      <span>Disponível para compra:</span>
+                      <img src={card.streaming} alt="Streaming" className="streaming-image" />
+                    </div>
+                    <div className="classification">
+                      <span> Crítica</span>
+                    </div>
+                    <div className="rating">
+                      {renderStarRating(card.rating)}
+                    </div>
+                  </>
+                )}
+
+                <div className="section-buttons">
+                  <button onClick={() => handleVerDetalhesClick(card.id)}>
+                    {expandedId === card.id ? <img src={closeButton} alt="" className="closeButton" /> : "Ver Detalhes"}
+                  </button>
+                </div>
+              </div>
+            </div>
+          </Container>
+        ))}
+      </CardsWrapper>
+    </div>
+  );
+};
+
+export default Cards;
--- src/components/cards/styles.ts
+++ src/components/cards/styles.ts
@@ -1,64 +1,236 @@
-import styled from "styled-components"
-// import { DEVICE_BREAKPOINTS } from "../../styles"
+import styled from "styled-components";
+import { DEVICE_BREAKPOINTS } from "../../styles";
+
+export const CardsWrapper = styled.div`
+  display: flex;
+  gap: 84px;
+  position: relative;
+`;
+
+export const FilterContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+  gap: 10px;
+
+`;
+
+export const SortSelect = styled.select`
+  padding: 5px 10px;
+  border: none;
+  border-radius: 5px;
+  background-color: #000;
+  border: 1px solid red;
+  font-size: 16px;
+  color: red;
+  cursor: pointer;
+
+  &:focus {
+    outline: none;
+  }
+
+  option {
+    background-color: #000;
+    color: red;
+  }
+`;
+
+export const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.7);
+  z-index: 5;
+`;
 
 export const Container = styled.div`
-	display: inline-block;
-	position: relative;
+  display: flex;
+  position: relative;
+  z-index: 0;
 
-	.infos {
-		p {
-			padding-bottom: 20px;
-		}
-		
-		display: flex;
-		position: absolute;
-		top: 50%;
-		flex-direction: column;
-		align-items: center;
-		text-align: center;
-		padding: 20px;
-		height: 217px;
-		border-radius: 30px;
-		background: linear-gradient(180deg, 
-		#ff0000 0.00%, 
-		rgba(128,0,0,0.3 ) 100.00%);
+  .infos {
+    display: flex;
+    position: absolute;
+    top: 331px;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    padding: 20px;
+    height: 217px;
+    width: 289px;
+    border-radius: 30px;
+    background: linear-gradient(180deg, #ff0000 0%, rgba(128, 0, 0, 0.3) 100%);
+    transform: translateY(-50%);
+
+    .title {
+      flex-grow: 0;
+      font-size: 20px;
+      font-weight: bold;
+      color: #ffffff;
+    }
+
+    .description {
+      flex-grow: 1;
+      padding-top: 12px;
+      font-size: 12px;
+      line-height: 20px;
+      font-style: normal;
+      text-align: left;
+      color: #ffffff;
+    }
+
+    .section-buttons {
+      flex-grow: 0;
+      button {
+        font-size: 20px;
+        color: #ffffff;
+        background: none;
+        border: none;
+      }
+    }
   }
 
-	&.expanded {
-		img {
-			position: absolute;
-			z-index: 1;
-		}
+  &.expanded {
+    z-index: 10;
+    position: fixed;
+    top: 0;
+    left: 25%;
 
-		.infos {
-		.description {
-			display: none;
-		}
+    .title {
+      padding: 0 11px 11px 0;
+    }
 
-		.appears {
-			span {
-				display: flex;
-				height: 274px;
-				width: 171px;
-			}
-		}
-		
-		display: flex;
-		position: relative;
-		top: 50%;
-		width: 607px;
-    padding-left: 304px;
-		height: 438px;
-		background: linear-gradient(90deg, 
-		#ff0000 0.00%, 
-		#400e0e 100.00%);
-		font-family: "Axiforma";
-		font-size: 18px;
-		font-weight: bold;
-		line-height: 22px;
-		font-style: normal;
-		text-align: left;
-		align-items: flex-start;
+    .more-info{
+      flex-grow: 1;
+      font-size: 12px;
+    }
+
+    .heroImage {
+      position: absolute;
+      left: 0px;
+      top: 236px;
+      z-index: 1;
+    }
+
+    .infos {
+      padding: 33px 25px 0 78px;
+      top: 454px;
+      left: 57%;
+      display: flex;
+      position: relative;
+      width: 410px;
+      height: 438px;
+      background: linear-gradient(90deg, #ff0000 0%, #400e0e 100%);
+      align-items: flex-start;
+      font-size: 18px;
+
+      .appearances {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        gap: 3px;
+      }
+
+      .label-appearances {
+        display: flex;
+        text-align: left;
+        font-weight: bold;
+      }
+
+      .description {
+        display: none;
+      }
+
+      .title {
+        flex-grow: 0;
+        margin-bottom: 11px;
+        font-size: 30px;
+      }
+
+      .appears {
+        display: flex;
+        flex-grow: 1;
+        flex-direction: column;
+      }
+
+      .classification {
+        flex-grow: 0;
+        height: fit-content;
+        font-size: 26px;
+      }
+
+      .streaming{
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        padding: 10px 0;
+        gap: 5px;
+        font-size: 12px;
+      }
+
+      .section-buttons {
+        display: flex;
+        justify-content: flex-end;
+        width: 100%;
+        padding: 0 10px 25px 0;
+
+        .closeButton {
+          font-size: 20px;
+          color: #ffffff;
+          background: none;
+          border: none;
+          cursor: pointer;
+        }
+      }
+    }
+
+    @media (max-width: ${DEVICE_BREAKPOINTS.LG}) {
+      position: relative;
+      left: 0;
+      .heroImage {
+        position: absolute;
+        left: 0px;
+        top: 0px;
+        z-index: 1;
+      }
+      .infos {
+        top: 48%;
+        left: 0;
+        z-index: 2;
+        padding: 25px;
+        width: 289px;
+        background: linear-gradient(
+          180deg,
+          #ff0000 -42%,
+          rgba(128, 0, 0, 0.3) 100%
+        );
+        transform: translateY(-50%);
+        display: flex;
+        align-items: flex-start;
+        justify-content: center;
+        font-size: 16px;
+
+        .title {
+          flex-grow: 0;
+          font-size: 25px;
+          margin-bottom: 11px;
+        }
+
+        .section-buttons {
+          padding: 0;
+          flex-grow: 0;
+        }
+
+        .appears {
+          flex-grow: 1;
+          span {
+            width: fit-content;
+            height: fit-content;
+          }
+        }
+      }
+    }
   }
-	}
-`
+`;
