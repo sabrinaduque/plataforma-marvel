883e07bd998848b54fd95e2bb90480d48424945c
Sabrina Duque <sabrinanduque@hotmail.com>
2024-05-20 03:15:05 -0300

    Implementando pagina characters

--- src/pages/characters/characters.tsx
+++ src/pages/characters/characters.tsx
@@ -1,13 +1,15 @@
 import { Header, Carousel } from '../../components'
 import { Container, Content } from './styles'
+import data, { IDataCharacter } from "../../data/data-character"
 
 const Characters: React.FC = () => {
+  const dataCharacters: IDataCharacter = data
+
   return (
     <Container>
       <Header />
-
       <Content>
-        <Carousel />
+        <Carousel data={dataCharacters}/>
       </Content>
     </Container>
   )
